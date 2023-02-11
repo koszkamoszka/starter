5350bfe631e46f2b959d23fe7f5b6e44fe9fd9c5
koszkamoszka <laletina69@gmail.com>
2023-02-12 00:45:58 +0100

    initial commit

--- src/main/java/com/koszkamoszka/Main.java
+++ src/main/java/com/koszkamoszka/Main.java
@@ -1,7 +1,38 @@
 package com.koszkamoszka;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        String a = scanner.nextLine();
+        while (!a.isEmpty()) {
+            list.add(a);
+            a = scanner.nextLine();
+        }
+
+        String max = "";
+        for (String line : list) {
+
+            if (line.length() > max.length()) {
+
+                max = line;
+
+            }
+
+        }
+
+        System.out.println(max);
+// asd asdfgh as ghj
+
+
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
